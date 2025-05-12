6a18762c8e5e95f7526cdb71845bcb6d3acb0400
Mayank Pandey <81166719+codeblack1718@users.noreply.github.com>
2025-05-12 15:14:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ def hello():
     return "Hello from CI/CD pipeline!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
 
 # test: docker push to ACR
